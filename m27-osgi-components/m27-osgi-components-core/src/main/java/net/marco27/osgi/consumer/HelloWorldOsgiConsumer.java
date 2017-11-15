b3362f4a0b6c108d2e4f0e4b8585637162d4f32d
Marco Guastalli <marco.guastalli@netcentric.biz>
2017-11-15 11:50:26 +0100

    added Reference to other bundle

--- m27-osgi-components/m27-osgi-components-core/src/main/java/net/marco27/osgi/consumer/HelloWorldOsgiConsumer.java
+++ m27-osgi-components/m27-osgi-components-core/src/main/java/net/marco27/osgi/consumer/HelloWorldOsgiConsumer.java
@@ -1,13 +1,16 @@
-package net.marco27.osgi.helloworld;
+package net.marco27.osgi.consumer;
 
 import java.util.Dictionary;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
+
+import net.marco27.osgi.helloworld.HelloWorldOsgiService;
 
 @Component(metatype = true, immediate = true, label = "Hello OSGi Component")
 @Property(name = HelloWorldOsgiConsumer.PROPERTY_NAME, value = { HelloWorldOsgiConsumer.PROPERTY_DEFAULT_VALUE })
@@ -20,10 +23,15 @@ public class HelloWorldOsgiConsumer {
 
     private String privateProperty;
 
+    @Reference
+    private HelloWorldOsgiService helloWorldOsgiService;
+
     @Activate
     protected void activate(BundleContext bundleContext, ComponentContext componentContext) {
         this.bundleContext = componentContext.getBundleContext();
         Dictionary<?, ?> properties = componentContext.getProperties();
         this.privateProperty = PropertiesUtil.toString(properties.get(PROPERTY_NAME), PROPERTY_DEFAULT_VALUE);
+
+        helloWorldOsgiService.helloWorldOsgi();
     }
 }
